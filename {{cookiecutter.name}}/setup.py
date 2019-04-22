451d14d74eb6557df530295725a73428b5ac1c36
Yoav Kleinberger <haarcuba@gmail.com>
2019-04-22 11:38:38 +0300

    initial commit

---
+++ {{cookiecutter.name}}/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+README = '{{cookiecutter.description}}'
+
+requires = []
+tests_require = [
+        'pytest',
+        'coverage'
+        ]
+
+setup(name='{{cookiecutter.name}}',
+      version='0.0.1',
+      description=README,
+      long_description=README,
+      package_dir={'': 'source'},
+      classifiers=[
+          "Programming Language :: Python",
+      ],
+      author='Yoav Kleinberger',
+      author_email="haarcuba@gmail.com",
+      packages=find_packages('source'),
+      include_package_data=True,
+      zip_safe=False,
+      extras_require={
+          'testing': tests_require,
+      },
+      install_requires=requires,
+      entry_points={
+          'console_scripts': [ '{{cookiecutter.name}} = {{cookiecutter.name}}.main:main' ]
+      },
+      )
